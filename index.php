44186c7101d20090c8d159d6951e0c84e550b227
Romoli-Gabriele <gh05romoli@gmail.com>
2021-10-20 08:37:10 +0200

    primo commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Master Mind
+    </title>
+</head>
+<body>
+    <h1>Gioco del Master Mind</h1>
+    <br>
+    <br>
+    <?php //rosso = 1, giallo = 2, verde = 3, celeste = 4
+    $colore1 = random_int(1,4);
+    $colore2 = random_int(1,4);
+    $colore3 = random_int(1,4);
+    $colore4 = random_int(1,4);
+    ?>
+</body>
+</html>
