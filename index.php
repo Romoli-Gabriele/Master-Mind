1ed270ffbb9e2d1f78e4c904336b671c2282ff88
Romoli-Gabriele <gh05romoli@gmail.com>
2021-10-20 08:57:57 +0200

    tabella

--- index.php
+++ index.php
@@ -9,11 +9,36 @@
     <h1>Gioco del Master Mind</h1>
     <br>
     <br>
-    <?php //rosso = 1, giallo = 2, verde = 3, celeste = 4
-    $colore1 = random_int(1,4);
-    $colore2 = random_int(1,4);
-    $colore3 = random_int(1,4);
-    $colore4 = random_int(1,4);
+    <table border="2">
+    <?php
+    session_start();
+     //rosso = 1, giallo = 2, verde = 3, celeste = 4
+    $_SESSION["soluzione"] = array( random_int(1,4), random_int(1,4),  random_int(1,4),  random_int(1,4));
+    $_SESSION["$b1"] = 1;
+    $_SESSION["$b2"] = 1;
+    $_SESSION["$b3"] = 1;
+    $_SESSION["$b4"] = 1;
+    echo '
+        <tr>
+            <td>
+                <button><img src="$"></button>
+            </td>
+            <td>
+                <button><img src="rosso.gif"></button>
+            </td>
+            <td>
+                <button><img src="rosso.gif"></button>
+            </td>
+            <td>
+                <button><img src="rosso.gif"></button>
+            </td>
+            <td>
+                <button><img src="spunta.gif"></button>
+            </td>
+        </tr>
+    ';
+    
     ?>
+    </table>
 </body>
 </html>
