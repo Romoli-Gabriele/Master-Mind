7ab579387bc68e5f458cb15449b72d5ad277ecaa
gh05romoli@gmail.com <gh05romoli@gmail.com>
2021-12-07 09:48:00 +0100

    da finire

--- MasterMind.php
+++ MasterMind.php
@@ -1,48 +1,86 @@
-<?php 
-class MasterMind{
+<?php
+class MasterMind
+{
 
     public $turno;
     public $soluzione;
 
-function __construct()
-{
-    $this->turno = 0;
-    $this->soluzione  = array( random_int(1,4), random_int(1,4),  random_int(1,4),  random_int(1,4));
-
-}
-
-function selectColor($i){
-    echo "<select name='$i'>
-        <option value='1'><img src='images/rosso.gif'></option>
-        <option value='2'><img src='images/giallo.gif'></option>
-        <option value='3'><img src='images/verde.gif'></option>
-        <option value='4'><img src='images/blu.gif'></option>
-    </select>";
-}
-
-function requestTable(){
-echo '
-    <tr>';
-    for($i =0; $i<4; $i++){
-        echo '<td>';
-            $this->selectColor($i);
-        echo '</td>';
-    }   
-        echo'
+    function __construct()
+    {
+        $this->turno = 0;
+        $this->soluzione  = array(random_int(1, 4), random_int(1, 4),  random_int(1, 4),  random_int(1, 4));
+    }
+    static function requestTable()
+    {
+        echo '<tr>';
+        for ($i = 0; $i < 4; $i++) {
+            echo '<td>';
+            echo "<select name='$i'>
+        <option value='1'  style='color: red;'>Rosso</option>
+        <option value='2' style='color: yellow;'>Giallo</option>
+        <option value='3' style='color: green';>Verde</option>
+        <option value='4' style='color: blue'>Blu</option>
+        </select>";
+            echo '</td>';
+        }
+        echo '
         <td>
-            <button type="submit"><img src="spunta.gif"></button>
+            <button type="submit"><img src="images/spunta.gif"></button>
         </td>
     </tr>
 ';
-}
-function winControl($sequenza){
-    if($sequenza[0] == $this->soluzione[0] && $sequenza[1] == $this->soluzione[1] && $sequenza[2] == $this->soluzione[2] && $sequenza[3] == $this->soluzione[3]){
-        return true;
-    }else{ 
-        return false;
     }
-}
-function calcolaColori($sequenza){
-    
-}
+    function winControl($sequenza)
+    {
+        if ($sequenza[0] == $this->soluzione[0] && $sequenza[1] == $this->soluzione[1] && $sequenza[2] == $this->soluzione[2] && $sequenza[3] == $this->soluzione[3]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function calcolaColori($sequenza)
+    {
+        $arr = ["nero" => 0, "bianco" => 0];
+        $i = 0;
+        foreach ($sequenza as $v) {
+            if ($v == $this->soluzione[$i]) {
+                $arr["nero"]++;
+            }
+            $i++;
+        }
+        foreach ($sequenza as $v) {
+            foreach ($this->soluzione as $k) {
+                if ($v == $k) {
+                    $arr["bianco"]++;
+                }
+            }
+        }
+        $arr["bianco"] -= $arr["nero"];
+        return $arr;
+    }
+    function resulTable($sequenza)
+    {
+        $arr = $this->calcolaColori($sequenza);
+        echo "<tr><td>$this->turno</td><td>";
+        foreach ($sequenza as $v) {
+            if ($v == 1) {
+                $img = "images/rosso.gif";
+            } elseif ($v == 2) {
+                $img = "images/giallo.gif";
+            } elseif ($v == 3) {
+                $img = "images/verde.gif";
+            } else {
+                $img = "images/blu.gif";
+            }
+            echo"<img src=$img>";
+        }
+        echo"</td><td>";
+        for($i=0;$i<$arr["nero"];$i++){
+            echo"<img src='images/nero.gif'>";
+        }
+        for($i=0;$i<$arr["bianco"];$i++){
+            echo"<img src='images/bianco.gif'>";
+        }
+        echo "</td></tr>";
+    }
 }
--- index.php
+++ index.php
@@ -10,24 +10,33 @@
     <br>
     <br>
     <table border="2">
-        <form method="POST">
+        <form method="POST" action="index.php">
     <?php
     require "MasterMind.php";
     session_start();
-    if(!isset($_SESSION["sessione"])){
-     //rosso = 1, giallo = 2, verde = 3, celeste = 4
+    if(!isset($_SESSION["soluzione"])){
+     //rosso = 1, giallo = 2, verde = 3, blu = 4
     $_SESSION["soluzione"] = new MasterMind();
+    var_dump($_SESSION["soluzione"]->soluzione);
     }else{
-        $sequenza = array($_POST[0], $_POST[1], $_POST[2], $_POST[3] );
-       if($_SESSION["soluzione"]:: winControl($sequenza)){
-            echo "<h1>Hai vinto, ci hai impiegato $_SESSION[soluzione]::$turno</h1>";
-       }else{
-
+        $sequenza = array($_POST["0"], $_POST["1"], $_POST["2"], $_POST["3"] );
+       if($_SESSION["soluzione"]-> winControl($sequenza)){
+           die("<h1>Hai vinto, ci hai impiegato $_SESSION[soluzione]::$turno</h1>");
+           session_destroy();
+       }
+       if($_SESSION["soluzione"]->turno>20){
+        die("<h1>Hai Perso, hai terminato i 20 turni");
+        session_destroy();
        }
     }
-    
+    $_SESSION["soluzione"]->requestTable();
     ?>
         </form>
+    </table>
+    <br>
+    <br>
+    <table>
+        <?php if(!isset($_SESSION["soluzione"])){ $_SESSION["soluzione"]->resulTable($sequenza);}?>
     </table>
 </body>
 </html>
